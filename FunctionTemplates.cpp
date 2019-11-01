da6eec05736fadf545bc597b5e296c71a2ea6f7a
markwhittyunsw <m.whitty@unsw.edu.au>
2019-11-01 13:13:15 +1100

    Created FunctionTemplates.cpp
    new example

---
+++ FunctionTemplates.cpp
@@ -0,0 +1,22 @@
+// function templates
+#include <iostream>
+
+template <class T> T sum (T a, T b)
+{
+  T result;
+  result = a + b;
+  return result;
+}
+
+int main () {
+  int i=5, j=6, k;
+  double f=2.0, g=0.5, h;
+  
+  k=sum<int>(i,j);
+  h=sum<double>(f,g);
+  
+  std::cout << k << std::endl;
+  std::cout << h << std::endl;
+  
+  return 0;
+}
